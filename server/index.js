6d89296be7335eef0009d0108c7ab1ef6881113f
oxana-ermolenko <oxana.ermolenko.dev@gmail.com>
2020-07-13 22:47:22 -0700

    paypalButton added

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ const config = require("./config/key")
 const mongoose = require("mongoose")
 const connect = mongoose.connect(config.mongoURI,
   {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+    useNewUrlParser: true, useUnifiedTopology: true
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -50,5 +49,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
+  console.log(`Server Runing at ${port}`)
 })
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const moment = require("moment");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+const moment = require("moment")
 
 const userSchema = mongoose.Schema({
     name: {
@@ -50,10 +50,10 @@ userSchema.pre('save', function( next ) {
     if(user.isModified('password')){    
         // console.log('password changed')
         bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
+            if(err) return next(err)
 
             bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
+                if(err) return next(err)
                 user.password = hash 
                 next()
             })
@@ -65,21 +65,21 @@ userSchema.pre('save', function( next ) {
 
 userSchema.methods.comparePassword = function(plainPassword,cb){
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err);
+        if (err) return cb(err)
         cb(null, isMatch)
     })
 }
 
 userSchema.methods.generateToken = function(cb) {
-    var user = this;
+    var user = this
     var token =  jwt.sign(user._id.toHexString(),'secret')
-    var oneHour = moment().add(1, 'hour').valueOf();
+    var oneHour = moment().add(1, 'hour').valueOf()
 
-    user.tokenExp = oneHour;
+    user.tokenExp = oneHour
     user.token = token;
     user.save(function (err, user){
         if(err) return cb(err)
-        cb(null, user);
+        cb(null, user)
     })
 }
 
@@ -87,9 +87,9 @@ userSchema.statics.findByToken = function (token, cb) {
     var user = this;
 
     jwt.verify(token,'secret',function(err, decode){
-        user.findOne({"_id":decode, "token":token}, function(err, user){
-            if(err) return cb(err);
-            cb(null, user);
+        user.findOne({"_id": decode, "token": token}, function(err, user){
+            if(err) return cb(err)
+            cb(null, user)
         })
     })
 }
